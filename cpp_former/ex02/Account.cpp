caa972ba0f24a742c4866e978a9968d01a2b5b28
sabe-0524 <soichiro-0524@icloud.com>
2025-06-08 17:06:08 +0900

    wip:ex02

---
+++ cpp_former/ex02/Account.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabe <sabe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/08 15:05:49 by sabe              #+#    #+#             */
+/*   Updated: 2025/06/08 17:03:42 by sabe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <time.h>
+#include <iostream>
+
+Account::Account(int initial_deposit)
+{
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_nbAccounts++;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+}
+
+int Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void Account::_displayTimestamp(void)
+{
+	struct tm when;
+	char buf[16];
+
+	time_t tme = time(NULL);
+	when = *localtime(&tme);
+	strftime(buf, 16, "%Y%m%d_%H%M%S", &when);
+	std::cout << "[" << buf << "]" << " ";
+}
+
+void Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawls:" << getNbWithdrawals << std::endl;
+}
+
+void Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawls:" << this->_nbWithdrawals << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
+	this->_nbDeposits++;
+	this->_amount += deposit;
+	std::cout << ";amount:" << this->_amount << ";nbDeposit:" << this->_nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawl) const
+{
+
+}
