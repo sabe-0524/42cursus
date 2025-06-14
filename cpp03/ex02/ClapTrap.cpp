ae2cb30bd61af2a47b682350fa4b6f37ff85396d
sabe-0524 <soichiro-0524@icloud.com>
2025-06-14 18:53:15 +0900

    start02

---
+++ cpp03/ex02/ClapTrap.cpp
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabe <sabe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 15:33:21 by sabe              #+#    #+#             */
+/*   Updated: 2025/06/14 18:52:45 by sabe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) : name("Unknown"), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << this->getClassName() << " Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string &name) : name(name), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << this->getClassName() << " string constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string &name, const unsigned int &hitPoints, const unsigned int &energyPoints, const unsigned int &attackDamage) : name(name), hitPoints(hitPoints), energyPoints(energyPoints), attackDamage(attackDamage)
+{
+	std::cout << this->getClassName() << " full constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) : name(other.name), hitPoints(other.hitPoints), energyPoints(other.energyPoints), attackDamage(other.attackDamage)
+{
+	std::cout << this->getClassName() <<" Copy constructor called" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator = (const ClapTrap &other)
+{
+	std::cout << this->getClassName() << " Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		this->name = other.name;
+		this->hitPoints = other.hitPoints;
+		this->energyPoints = other.energyPoints;
+		this->attackDamage = other.attackDamage;
+	}
+	return (*this);
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << this->getClassName() << " Destructor called" << std::endl;
+}
+
+void ClapTrap::setName(const std::string &name)
+{
+	this->name = name;
+}
+
+void ClapTrap::setHitPoints(const unsigned int &hitPoints)
+{
+	this->hitPoints = hitPoints;
+}
+
+void ClapTrap::setEnergyPoints(const unsigned int &energyPoints)
+{
+	this->energyPoints = energyPoints;
+}
+
+void ClapTrap::setAttackDamage(const unsigned int &attackDamage)
+{
+	this->attackDamage = attackDamage;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->hitPoints == 0 || this->energyPoints == 0)
+		std::cout << this->getClassName() << " " << this->name << " can't attack." << std::endl;
+	else
+	{
+		std::cout << this->getClassName() << " " << this->name << " attacks " << target << " , causing " << this->attackDamage << " points of damage!" << std::endl;
+		this->energyPoints--;
+	}
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->hitPoints == 0)
+	{
+		std::cout << this->getClassName() << " " << this->name << " has already died." << std::endl;
+		return;
+	}
+	std::cout << this->getClassName() << " " << this->name << " is taken " << amount << " damage." << std::endl;
+	if (this->hitPoints < amount)
+		this->hitPoints = 0;
+	else
+		this->hitPoints -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->hitPoints == 0 || this->energyPoints == 0)
+		std::cout << this->getClassName() << " " << this->name << " can't be repaired." << std::endl;
+	else
+	{
+		std::cout << this->getClassName() << " " << this->name << " is repaired " << amount << " points!" << std::endl;
+		this->hitPoints += amount;
+		this->energyPoints--;
+	}
+}
---
+++ cpp03/ex02/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabe <sabe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 16:12:10 by sabe              #+#    #+#             */
+/*   Updated: 2025/06/14 17:22:27 by sabe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	protected:
+		const char* getClassName() const { return "ScavTrap"; }
+	public:
+		ScavTrap(void);
+		ScavTrap(const std::string &name);
+		ScavTrap(const ScavTrap &other);
+		ScavTrap &operator=(const ScavTrap &other);
+		virtual ~ScavTrap(void);
+
+		void guardGate(void);
+};
+
+#endif
